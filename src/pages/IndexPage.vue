a1658fbdde1afb8d1aafb5cfa1a232078ebcb0c5
monthakan-w <Monthakan4386@gmail.com>
2025-10-08 14:12:20 +0700

    reset HEAD~1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,7 +9,7 @@
       <q-input
         filled
         v-model="name"
-        label="ชื่อ-นามสกุล *"
+        label="ชื่อ-สกุล *"
         hint="ชื่อและนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ']"
